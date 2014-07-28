192945d2cd379f54fa578d9b22053283655e46f4
Tom Barrett <tom@easyjethro.com.au>
2014-07-28 20:36:39 +1000

    Moving creation of member view to the installer, since it did not find it in the members db_ojects folder

--- include/installer.class.php
+++ include/installer.class.php
@@ -12,7 +12,7 @@ class Installer
 
 	function run()
 	{
-		$sql = 'SELECT count(*) FROM person';
+		$sql = 'SELECT count(*) FROM _person';
 		$res = $GLOBALS['db']->queryOne($sql);
 		if (!PEAR::isError($res)) {
 			trigger_error('System has already been installed, installer is aborting');
@@ -155,7 +155,32 @@ class Installer
 			  getCurrentUserID() IS NOT NULL
 			  AND
 			  (NOT EXISTS (SELECT * FROM account_group_restriction gr WHERE gr.personid  = getCurrentUserID())
-				   OR g.id IN (SELECT groupid FROM account_group_restriction gr WHERE gr.personid = getCurrentUserID()))"
+				   OR g.id IN (SELECT groupid FROM account_group_restriction gr WHERE gr.personid = getCurrentUserID()))",
+
+			'CREATE VIEW member AS
+			SELECT mp.id, mp.first_name, mp.last_name, mp.gender, mp.age_bracket, mp.congregationid,
+			mp.email, mp.mobile_tel, mp.work_tel, mp.familyid,
+			mf.family_name, mf.addresss_street, mf.address_suburb, mf.address_state, mf.address_postcode, mf.home_tel
+			FROM _person mp
+			JOIN family mf ON mf.id = mp.familyid
+			JOIN person_group_membership pgm1 ON pgm1.personid = mp.id
+			JOIN _person_group pg ON pg.id = pgm1.groupid AND pg.share_member_details = 1
+			JOIN person_group_membership pgm2 ON pgm2.groupid = pg.id
+			JOIN _person up ON up.id = pgm2.personid
+			WHERE up.personid = getCurrentUserID() AND up.status <> "archived"
+			/* archived persons cannot see members of any group */
+			UNION
+
+			SELECT mp.id, mp.first_name, mp.last_name, mp.gender, mp.age_bracket, mp.congregationid,
+			mp.email, mp.mobile_tel, mp.work_tel, mp.familyid,
+			mf.family_name, mf.address_street, mf.address_suburb, mf.address_state, mf.address_postcode, mf.home_tel
+			FROM _person mp
+			JOIN family mf ON mf.id = mp.familyid
+			JOIN _person self ON self.familyid = mp.familyid
+			WHERE self.id = getCurrentUserID()
+			AND ((self.status <> "archived") OR (mp.id = self.id))
+			/* archived persons can only see themselves, not any family members */
+			;'
 		);
 		foreach ($sql as $s) {
 			$r = $GLOBALS['db']->query($s);
