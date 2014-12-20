9271a527cc508ae6149576b12208ac2838dc163e
Tom Barrett <tom@easyjethro.com.au>
2014-12-20 15:46:52 +1100

    Roster CSV

--- calls/call_roster_csv.class.php
+++ calls/call_roster_csv.class.php
@@ -7,7 +7,7 @@
  * @version $Id: call_display_roster.class.php,v 1.2 2013/03/19 09:47:51 tbar0970 Exp $
  * @package jethro-pmm
  */
-class Call_Display_Roster extends Call
+class Call_Roster_CSV extends Call
 {
 	/**
 	 * Execute this call
@@ -17,54 +17,15 @@ class Call_Display_Roster extends Call
 	 */
 	function run()
 	{
-		$roster_id = (int)array_get($_REQUEST, 'viewid');
+		$roster_id = (int)array_get($_REQUEST, 'roster_view');
 		if (empty($roster_id)) return;
 		$view = $GLOBALS['system']->getDBObject('roster_view', $roster_id);
-
-		?>
-		<html>
-			<head>
-				<style media="print">
-					html body * {
-						color: black;
-						text-decoration: none;
-					}
-				</style>
-				<style>
-					* {
-						font-family: sans-serif;
-					}	
-					td, th {
-						padding: 3px 1ex;
-						font-size: 0.8em;
-					}
-					th {
-						background-color: #555;
-						color: white;
-					}
-					th * {
-						color: white !important;
-					}
-					table {
-						border-collapse: collapse;
-					}
-					.smallprint {
-						margin-top: 1ex;
-						font-size: 75%;
-					}
-				</style>
-			</head>
-			<body>
-				<h1>Roster: <?php $view->printFieldValue('name'); ?></h1>
-				<?php
-
-				$start_date = substr(array_get($_REQUEST, 'start_date', ''), 0, 10);
-				$end_date = substr(array_get($_REQUEST, 'end_date', ''), 0, 10);
-				$view->printView($start_date, $end_date, FALSE, TRUE);
-				?>
-			</body>
-		</html>
-		<?php
+		$start_date = substr(array_get($_REQUEST, 'start_date', ''), 0, 10);
+		$end_date = substr(array_get($_REQUEST, 'end_date', ''), 0, 10);
+		header('Content-type: text/csv');
+		header('Content-disposition: attachment; filename='.preg_replace('[^a-zA-Z0-9]', '_', $view->getValue('name')));
+		$view->printCSV($start_date, $end_date);
+		
 	}
 }
 ?>
