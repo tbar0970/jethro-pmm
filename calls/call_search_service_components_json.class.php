9725b166140ae35bcd5f386d0149a5a68407b8a2
Tom Barrett <tom@tombarrett.id.au>
2015-02-10 16:04:54 +1100

    Fix filtering of service components

--- calls/call_search_service_components_json.class.php
+++ calls/call_search_service_components_json.class.php
@@ -3,19 +3,8 @@ class Call_Search_Service_Components_Json extends Call
 {
 	function run()
 	{
-		$conds = Array();
-		if (!empty($_REQUEST['search'])) {
-			$s = $_REQUEST['search'];
-			$conds += Array(
-						'title' => '%'.$s.'%',
-						'alt_title' => '%'.$s.'%',
-						'content_html' => '%'.$s.'%',
-					);
-		}
-		if (!empty($_REQUEST['tagid'])) {
-			$conds['tagid'] = (int)$_REQUEST['tagid'];
-		}
-		$results = $GLOBALS['system']->getDBObjectData('service_component', $conds, 'OR');
+		$GLOBALS['system']->includeDBClass('service_component');
+		$results = Service_Component::search(array_get($_REQUEST, 'search'), array_get($_REQUEST, 'tagid'), array_get($_REQUEST, 'congregationid'));
 
 		echo '['.implode(',', array_keys($results)).']';
 	}
