18d876725beda19fdf6bb834392f33b45e9bd4e5
Tom Barrett <tom@easyjethro.com.au>
2015-04-17 13:43:24 +1000

    Updated the update-checker

--- views/view_10_admin__6_upgrade.class.php
+++ views/view_10_admin__6_upgrade.class.php
@@ -18,8 +18,22 @@ class View_Admin__Upgrade extends View
 	function printView()
 	{
 		?>
-		<iframe frameborder="0" style="width: 100%; height: 400px" src="http://jethro-pmm.sourceforge.net/update_check.php?currentversion=<?php echo JETHRO_VERSION; ?>"></iframe>
+		<p id="message"></p>
+		<script>
+			$.ajax('https://api.github.com/repos/tbar0970/jethro-pmm/tags', {
+				dataType: 'json'
+			}).done(function (data) {
+				if (data[0].name == '<?php echo JETHRO_VERSION; ?>') {
+					$('#message').html('<i class="icon-ok"></i> Your system is up to date on version '+data[0].name);
+				} else {
+					$('#message').html('<i class="icon-warning-sign"></i>Your system is running <?php echo JETHRO_VERSION; ?> but <a href="https://github.com/tbar0970/jethro-pmm/releases" target="_blank">'+data[0].name + '</a> is available');
+				}
+			});
+		</script>
 		<?php
+		if (defined('SYSADMIN_HREF') && strlen(SYSADMIN_HREF)) {
+			echo '<p>For help, <a href="'.SYSADMIN_HREF.'">contact your system administrator</a></p>';
+		}
 	}
 }
 ?>
