7a17c7832ba905fc86522ef10f7d440c9bf909ea
Tom Barrett <tom@tombarrett.id.au>
2015-05-01 22:04:31 +1000

    Tidy up

--- db_objects/headcount.class.php
+++ db_objects/headcount.class.php
@@ -9,14 +9,12 @@ class Headcount
 					`congregationid` INT(11) NOT NULL,
 					`number` INT(11) NOT NULL,
 					PRIMARY KEY (`date`, `congregationid`)
-					/* CONSTRAINT FOREIGN KEY (`congregationid`) REFERENCES `congregation` (`id`) */
 				) Engine=InnoDB;',
 			 'CREATE TABLE person_group_headcount (
 					`date` DATE NOT NULL,
 					`person_groupid` INT(11) NOT NULL,
 					`number` INT(11) NOT NULL,
 					PRIMARY KEY (`date`, `person_groupid`)
-					/* CONSTRAINT FOREIGN KEY (`person_groupid`) REFERENCES `_person_group` (`id`) */
 				) Engine=InnoDB;'
 		);
 	}
