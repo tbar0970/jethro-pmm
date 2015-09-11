fdd66545477f0462e8ad8849e0d69905aa89512e
tbar0970 <tom@easyjethro.com.au>
2015-09-11 16:36:15 +1000

    Cleanup

--- upgrades/2015-upgrade-to-2.11_ical.sql
+++
@@ -1,7 +0,0 @@
-/* iCal subscriptions */
-CREATE TABLE person_uuid (
-    personid INT PRIMARY KEY,
-    uuid VARCHAR(64) NOT NULL,
-    CONSTRAINT UNIQUE INDEX (uuid),
-    constraint person_uuid_fk foreign key (personid) references _person (id) on delete cascade
-) ENGINE=InnoDB;
