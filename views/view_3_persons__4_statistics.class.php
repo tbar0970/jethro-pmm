770239cb843519824eebdb09744b3c3dae8b59dd
Edward Whiting <edwhiting@bigpond.com>
2016-02-28 07:46:57 +1100

    Refactored to use Angular.

--- views/view_3_persons__4_statistics.class.php
+++ views/view_3_persons__4_statistics.class.php
@@ -1,5 +1,13 @@
 <?php
-
+/* This class is a holder for the data that gets sent to the client. */
+class PersonStatistic{
+	public $statusName;
+	public $count;
+	public function __construct(){
+		$this->count = 0;
+		$this->statusName = "";
+	}
+}
 class View_Persons__Statistics extends View
 {
 	static function getMenuPermissionLevel()
@@ -16,19 +24,28 @@ class View_Persons__Statistics extends View
 	{
 		$GLOBALS['system']->includeDBClass('person');
 		$stats = Person::getStatusStats();
-		?>
-		<table class="table table-auto table-striped table-bordered">
-		<?php
+		$personStats = Array();
+		
 		foreach ($stats as $status_name => $count) {
-			?>
-			<tr>
-				<th><?php echo ents($status_name); ?></th>
-				<td><?php echo (int)$count; ?></td>
-			</tr>
-			<?php
+			$p = new PersonStatistic();
+			$p->statusName = ents($status_name);
+			$p->count = (int)$count;
+			array_push($personStats,$p);
 		}
+		$personStatsJson = json_encode($personStats);
 		?>
+		<table ng-controller="ViewPersonsStatistics" class="table table-auto table-striped table-bordered">
+			<tr ng-repeat="p in personStats">
+				<th>{{p.statusName}}</th>
+				<td>{{p.count}}</td>
+			</tr>
 		</table>
+		<script>
+			/* mainTemplateApp is initialised in main.template.php. */
+			mainTemplateApp.controller("ViewPersonsStatistics",function($scope){
+				$scope.personStats = <?php echo $personStatsJson ?>;
+			});
+		</script>
 		<?php
 	}
 
