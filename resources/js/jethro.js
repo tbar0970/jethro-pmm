4c7a5cb34a8cb98cc36d68e9448ae432e019ae57
Nicholas van Rheede van Oudtshoorn <vanoudt@gmail.com>
2016-03-03 14:20:23 +0800

    Remove disused code (Originally from upstream)

--- resources/js/jethro.js
+++ resources/js/jethro.js
@@ -177,19 +177,6 @@ $(document).ready(function () {
     $($(this).parents('tr')[0]).find('div.select-rule-options').css('display', (this.checked ? '' : 'none'));
   });
 
-  if ($('#datefield-rules')) {
-    $('.datefield-rule-period').hide();
-    $('.datefield-rule-criteria').change(function () {
-      if ((this.value === 'exact') || (this.value === 'anniversary')) {
-        $(this).siblings('.datefield-rule-period').show();
-      } else {
-        $(this).siblings('.datefield-rule-period').hide();
-      }
-    }).change();
-  }
-
-
-
   /************************ SEARCH CHOOSERS ************************/
 
   $('input.person-search-multiple').each(function () {
