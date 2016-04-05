0d0abf257143fa2851ff9090591eb32142812b47
Tom Barrett <tom@tombarrett.id.au>
2016-04-05 12:39:43 +1000

    Make TH text not bold - it's too much contrast

--- resources/less/jethro.less.php
+++ resources/less/jethro.less.php
@@ -60,11 +60,13 @@ if (is_readable($confFile)) {
 	background-image: linear-gradient(to bottom, @jethroDarkish, @jethroDarkest);
 	background-repeat: repeat-x;
 	filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ffA6ACC2', endColorstr='#ff858A9B', GradientType=0);
+}
+.table thead th, .table tfoot th, .table thead th *, .table tfoot th * {
+	/* use a colour that will contrast with the background */
 	color: contrast(@jethroDarkish, @jethroDarkText, @jethroLightText);
+	font-weight: normal !important; /* bold makes the contrast too stark */
 }
-.table thead th *, .table tfoot th * {
-	color: @jethroDarkText !important;
-}
+
 /* the styling used for focused textboxes, also applied to the "active" note when viewing a person */
 .highlight {
 	-webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(256, 0, 0, 0.9);
