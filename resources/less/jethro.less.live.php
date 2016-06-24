8f6058a1b277065dd6e7a105671bd275e9f717dd
Nicholas van Oudtshoorn <vanoudt@gmail.com>
2016-06-24 17:58:01 +0800

    Make "live" CSS cache for 1 day using browser caching

--- resources/less/jethro.less.live.php
+++ resources/less/jethro.less.live.php
@@ -2,13 +2,19 @@
 require_once '../lessphp/lib/Less/Autoloader.php';
 
 Less_Autoloader::register();
-
-header('Content-Type: text/css');
 $parser = new Less_Parser();
 ob_start();
 include './jethro.less.php';
 $less_css = ob_get_clean();
-//echo $less_css;
 $parser->parse($less_css);
+
+if (defined('TIMEZONE') && constant('TIMEZONE')) {                                                                                                           
+	        date_default_timezone_set(constant('TIMEZONE'));  
+}
+$datetime = new DateTime();
+$datetime->add(new DateInterval('P1D'));
+$tsstring = gmdate('D, d M Y H:i:s ', $datetime->getTimestamp()) . 'GMT';
+header('Content-Type: text/css');                                                                                                                            
+header("Expires: " . $tsstring);
 echo $parser->getCss();
 ?>
