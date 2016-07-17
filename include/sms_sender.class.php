579c89b4aecb363e51aad6ddf35d4f25c0512541
Tom Barrett <tom@tombarrett.id.au>
2016-07-17 19:56:31 +1000

    Fix #218 - set_error_handler(NULL) not compatible with PHP < 5.5

--- include/sms_sender.class.php
+++ include/sms_sender.class.php
@@ -55,8 +55,14 @@ Class SMS_Sender
 				'header' => $header
 			)
 		);
-		// To work with HTTP Server errors ourselves, override the system error_handler
-		set_error_handler(null);
+		// Convert errors to exceptions so we can catch them
+		set_error_handler(function($errno, $errstr, $errfile, $errline, array $errcontext) {
+			// error was suppressed with the @-operator
+			if (0 === error_reporting()) {
+				return false; 
+			}
+			throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+		});
 		try {
 			$fp = @fopen(SMS_HTTP_URL, 'r', false, stream_context_create($opts));
 			if (!$fp) {
