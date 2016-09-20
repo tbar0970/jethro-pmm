bfcda26a59a30c4218a788514286b0d966306d47
Tom Barrett <tom@tombarrett.id.au>
2016-09-21 08:14:28 +1000

    Fix #269 by allowing zooming on ipad

--- templates/head.template.php
+++ templates/head.template.php
@@ -5,8 +5,12 @@
 		if (isset($GLOBALS['system']) && ($title = $GLOBALS['system']->getTitle())) echo ' - '.$title;
 		?>
 	</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0
+<?php
+if (FALSE === strpos($_SERVER['HTTP_USER_AGENT'], 'iPad')) echo ', user-scalable=no';
+?>
+		  ">
 	<!--[if IE]>
 	<link type="text/css" rel="stylesheet" href="<?php echo BASE_URL; ?>resources/css/jethro_msie.css" />
 	<![endif]-->
