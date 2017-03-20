a258306537a19b3a6271778f475fdeed75b0a8b4
tbar0970 <tom@easyjethro.com.au>
2017-03-20 15:36:36 +1100

    Script to trigger the settings migration

---
+++ scripts/migrate_settings.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This script can be used to trigger the migration of settings from conf.php
+ * to the database (https://github.com/tbar0970/jethro-pmm/issues/15)
+ *
+ * It is not NECESSARY to run this script - you can instead just load Jethro
+ * in the browser to trigger the migration.  But sometimes it's conveient to be
+ * able to do it (in bulk, pehaps) from the command line.
+ */
+
+ini_set('display_errors', 1);
+define('JETHRO_ROOT', dirname(dirname(__FILE__)));
+set_include_path(get_include_path().PATH_SEPARATOR.JETHRO_ROOT);
+if (!is_readable(JETHRO_ROOT.'/conf.php')) {
+	trigger_error('Jethro configuration file not found.  You need to copy conf.php.sample to conf.php and edit it before Jethro can run', E_USER_ERROR);
+	exit();
+}
+require_once JETHRO_ROOT.'/conf.php';
+if (!defined('DSN')) define('DSN', constant('PRIVATE_DSN'));
+require_once JETHRO_ROOT.'/include/init.php';
+dump_messages();
