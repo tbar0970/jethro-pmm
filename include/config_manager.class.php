915f358b403ec74c51db2fdc80a56675644f7141
tbar0970 <tom@easyjethro.com.au>
2017-03-23 23:40:46 +1100

    Fix the fix of Issue #343 ...

--- include/config_manager.class.php
+++ include/config_manager.class.php
@@ -116,6 +116,7 @@ class Config_Manager {
 		$res = $db->exec($SQL);
 		check_db_result($res);
 
+		// Update references to age brackets in action plans
 		$SQL = 'SELECT id, actions FROM _disused_action_plan_backup';
 		$plans = $db->queryAll($SQL);
 		check_db_result($plans);
@@ -130,13 +131,17 @@ class Config_Manager {
 			}
 		}
 
+		// Update references to age brackets in person-report rules
 		$SQL = 'SELECT id, params FROM _disused_person_query_backup';
 		$queries = $db->queryAll($SQL);
 		check_db_result($queries);
 		foreach ($queries as $row) {
 			$params = unserialize($row['params']);
 			if (!empty($params['rules']['p.age_bracket'])) {
-				$params['rules']['p.age_bracketid'] = (int)$params['rules']['p.age_bracket']+1;
+				$params['rules']['p.age_bracketid'] = Array();
+				foreach ($params['rules']['p.age_bracket'] as $k => $v) {
+					$params['rules']['p.age_bracketid'][$k] = (int)$v+1;
+				}
 				unset($params['rules']['p.age_bracket']);
 				$SQL = 'UPDATE person_query SET params = '.$db->quote(serialize($params)).' WHERE id = '.(int)$row['id'];
 				$res = $db->exec($SQL);
--- scripts/fix_reports.php
+++ scripts/fix_reports.php
@@ -1,11 +1,8 @@
 <?php
 /**
- * This script can be used to trigger the migration of settings from conf.php
- * to the database (https://github.com/tbar0970/jethro-pmm/issues/15)
- *
- * It is not NECESSARY to run this script - you can instead just load Jethro
- * in the browser to trigger the migration.  But sometimes it's conveient to be
- * able to do it (in bulk, pehaps) from the command line.
+ * This script can be used to fix problems in reports following an upgrade to 2.19.0
+ * This script is ONLY needed if your system was upgraded to 2.19.0 exactly.
+ * If you upgraded from something before 2.19.0 to 2.19.1 or later, it is not necessary.
  */
 
 ini_set('display_errors', 1);
@@ -20,7 +17,7 @@ if (!defined('DSN')) define('DSN', constant('PRIVATE_DSN'));
 require_once JETHRO_ROOT.'/include/init.php';
 $db = $GLOBALS['db'];
 
-$SQL = 'CREATE TABLE _disused_person_query_backup SELECT * from person_query';
+$SQL = 'CREATE TABLE IF NOT EXISTS _disused_person_query_backup SELECT * from person_query';
 $res = $db->exec($SQL);
 check_db_result($res);
 
@@ -30,11 +27,15 @@ check_db_result($queries);
 foreach ($queries as $row) {
 	$params = unserialize($row['params']);
 	if (!empty($params['rules']['p.age_bracket'])) {
-		$params['rules']['p.age_bracketid'] = (int)$params['rules']['p.age_bracket']+1;
+		$params['rules']['p.age_bracketid'] = Array();
+		foreach ($params['rules']['p.age_bracket'] as $k => $v) {
+			$params['rules']['p.age_bracketid'][$k] = (int)$v+1;
+		}
 		unset($params['rules']['p.age_bracket']);
 		$SQL = 'UPDATE person_query SET params = '.$db->quote(serialize($params)).' WHERE id = '.(int)$row['id'];
 		$res = $db->exec($SQL);
 		check_db_result($res);
 	}
 }
+
 echo "done \n";
