6754c3c38161d0667e74485033a3be71596bd3b1
Nicholas van Oudtshoorn <vanoudt@gmail.com>
2017-05-22 16:46:11 +0800

    Use PDO for scripts

--- scripts/task_reminder.php
+++ scripts/task_reminder.php
@@ -15,7 +15,21 @@ if (!is_readable(JETHRO_ROOT.'/conf.php')) {
 }
 require_once JETHRO_ROOT.'/conf.php';
 
-if (!defined('DSN')) define('DSN', constant('PRIVATE_DSN'));
+if (defined('PRIVATE_DSN')) {
+        preg_match('|([a-z]+)://([^:]*)(:(.*))?@([A-Za-z0-9\.-]*)(/([0-9a-zA-Z_/\.]*))|',
+     PRIVATE_DSN,$matches);
+         if (!defined('DB_TYPE')) define('DB_TYPE', $matches[1]);
+         if (!defined('DB_HOST')) define('DB_HOST', $matches[5]);
+         if (!defined('DB_DATABASE')) define('DB_DATABASE', $matches[7]);
+         if (!defined('DB_PRIVATE_USERNAME')) define('DB_PRIVATE_USERNAME', $matches[2]);
+         if (!defined('DB_PRIVATE_PASSWORD')) define('DB_PRIVATE_PASSWORD', $matches[4]);
+}
+if (!defined('DSN')) {
+        define('DSN', DB_TYPE . ':host=' . DB_HOST . (!empty(DB_PORT)? (';port=' . DB_PORT):'') . ';dbname=' . DB_DATABASE . ';charset=utf8');
+}
+if (!defined('DB_USERNAME')) define('DB_USERNAME', DB_PRIVATE_USERNAME);
+if (!defined('DB_PASSWORD')) define('DB_PASSWORD', DB_PRIVATE_PASSWORD);
+
 require_once JETHRO_ROOT.'/include/init.php';
 
 if (ifdef('TASK_NOTIFICATION_ENABLED', FALSE) == FALSE) {
