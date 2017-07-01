5d813dec7f675bb7049617b6c940ddcd76e481c4
Nicholas van Oudtshoorn <vanoudt@gmail.com>
2017-07-01 18:35:28 +0800

    The exception object's getMessage function should be referenced using -> instead of ::

--- include/jethrodb.php
+++ include/jethrodb.php
@@ -20,7 +20,7 @@ class JethroDB extends PDO {
   }
 
   public function db_error($exception) {
-    trigger_error('Database Error in query.<br>( ' . $exception::getMessage() . ')', E_USER_ERROR);
+    trigger_error('Database Error in query.<br>( ' . $exception->getMessage() . ')', E_USER_ERROR);
     exit();
   }
   
