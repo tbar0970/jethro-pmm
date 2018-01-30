a6a6c801e6cf71882d5eb8cd2529dd63b86a31cd
tbar0970 <tom@easyjethro.com.au>
2018-01-30 22:43:51 +1100

    Fix Issue #440 by standardising collations. This fix resolves both existing databasees and the code that will create new ones.

--- upgrades/2018-upgrade-to-2.22.sql
+++ upgrades/2018-upgrade-to-2.22.sql
@@ -1,2 +1,5 @@
-/* Issue #440 - fix collection on family table */
- ALTER TABLE family convert to CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+/* Issue #440 - fix collations on several tables so they are all consistent */
+ALTER TABLE family convert to CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE note_comment convert to CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE db_object_lock convert to CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+ALTER TABLE _person CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
