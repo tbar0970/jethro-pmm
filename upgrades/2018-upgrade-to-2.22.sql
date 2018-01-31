d911a61570f33f280e4ff8d6e72190a02d210601
tbar0970 <tom@easyjethro.com.au>
2018-01-31 21:32:49 +1100

    Update empty setting where necssary

--- upgrades/2018-upgrade-to-2.22.sql
+++ upgrades/2018-upgrade-to-2.22.sql
@@ -3,3 +3,8 @@ ALTER TABLE family convert to CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 ALTER TABLE note_comment convert to CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 ALTER TABLE db_object_lock convert to CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 ALTER TABLE _person CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+/* update emty setting where necessary to avoid errors */
+UPDATE setting set value = 'SMS Sent'
+WHERE symbol = 'SMS_SAVE_TO_NOTE_SUBJECT'
+AND value = '';
