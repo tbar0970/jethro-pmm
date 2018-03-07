cc8669ee290f9000bb83e59dcdfa10a83507e640
tbar0970 <tom@easyjethro.com.au>
2018-03-07 18:57:44 +1100

    Resolves #452 - add the task notification settings to the installer and also get the upgrade script to make sure they're there in existing systems

--- upgrades/2018-upgrade-to-2.22.sql
+++ upgrades/2018-upgrade-to-2.22.sql
@@ -8,3 +8,16 @@ ALTER TABLE _person CONVERT TO CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 UPDATE setting set value = 'SMS Sent'
 WHERE symbol = 'SMS_SAVE_TO_NOTE_SUBJECT'
 AND value = '';
+
+/* fix #452 - using insert ignore because these settings MAY already be there */
+SET @newRank = (SELECT rank FROM setting WHERE symbol = 'SMTP_SERVER');
+
+UPDATE setting SET rank = rank + 10
+WHERE rank > @newRank-1;
+
+INSERT IGNORE INTO setting (rank, heading, symbol, note, type, value)
+VALUES
+(@newRank := @newRank+1, 'Task Notifications', 'TASK_NOTIFICATION_ENABLED', '(This feature also requires the task_reminder.php script to be called by cron every 5 minutes)', 'bool', 0),
+(@newRank := @newRank+1, '',                   'TASK_NOTIFICATION_FROM_NAME', 'Name from which task notifications should be sent', 'text', 'Jethro'),
+(@newRank := @newRank+1, '',                   'TASK_NOTIFICATION_FROM_ADDRESS', 'Email address from which task notifications should be sent', 'text', ''),
+(@newRank := @newRank+1, '',                   'TASK_NOTIFICATION_SUBJECT', '', 'text', 'New notes assigned to you');
