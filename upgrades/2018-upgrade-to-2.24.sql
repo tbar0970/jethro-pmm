52066122bd7790525313c470d06ccb0e30f136d0
tbar0970 <tom@easyjethro.com.au>
2018-07-07 21:39:32 +1000

    Fix #492 by correcting setting note. Fix #506 by correcting col definition for is_adult. Issue #457 - clean up zero dates for tidyiness.

---
+++ upgrades/2018-upgrade-to-2.24.sql
@@ -0,0 +1,9 @@
+/* Issue #457 - clean up zero dates just for tidyness */
+UPDATE _person SET status_last_changed = NULL where CAST(status_last_changed AS CHAR(20)) = '0000-00-00 00:00:00' ;
+
+/* issue #506 - is_adult col sometimes wrong definition */
+UPDATE age_bracket set is_adult = 0 where is_adult <> 1;
+ALTER TABLE age_bracket MODIFY COLUMN is_adult TINYINT(1) UNSIGNED NOT NULL DEFAULT 0;
+
+/* issue #492 - correct the wording for the RESTRICTED_USERS_CAN_ADD setting */
+UPDATE setting SET note = 'Can users with congregation restrictions add new persons and families?' where symbol = 'RESTRICTED_USERS_CAN_ADD';
