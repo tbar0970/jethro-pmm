6d8271c342c338d88761b8ed970461049da83208
tbar0970 <tom@easyjethro.com.au>
2019-12-02 14:06:12 +1100

    Combining the legacy merge and TBS merge. Updating the docs to suit.

--- calls/call_document_merge.class.php
+++ calls/call_document_merge.class.php
@@ -1,8 +1,13 @@
 <?php
-class Call_spreadsheet_merge extends Call
+class Call_Document_Merge extends Call
 {
 	function run()
 	{
+		if (array_get($_REQUEST, 'template_format') == 'legacy') {
+			$this->runLegacy();
+			exit;
+		}
+
 		$file_info = array_get($_FILES, 'source_document');
 		$content = null;
 		if (empty($file_info['tmp_name'])) {
@@ -29,17 +34,17 @@ class Call_spreadsheet_merge extends Call
 					date_default_timezone_set('UTC');
 				}
 				$TBS = new clsTinyButStrong;
-				$TBS->Plugin(TBS_INSTALL, OPENTBS_PLUGIN); 
+				$TBS->Plugin(TBS_INSTALL, OPENTBS_PLUGIN);
 				$TBS->SetOption('noerr', TRUE);
 				$TBS->LoadTemplate($source_file, OPENTBS_ALREADY_UTF8);
-				$TBS->ResetVarRef(false); 
+				$TBS->ResetVarRef(false);
 				$TBS->VarRef['x_num'] = 3152.456;
 				$TBS->VarRef['x_pc'] = 0.2567;
 				$TBS->VarRef['x_dt'] = mktime(13,0,0,2,15,2010);
 				$TBS->VarRef['x_bt'] = true;
 				$TBS->VarRef['x_bf'] = false;
 				$TBS->VarRef['x_delete'] = 1;
-				$TBS->VarRef['yourname'] = 'Some name';
+				$TBS->VarRef['yourname'] = $_SESSION['user']['username'];
 				$TBS->VarRef['system_name'] = ifdef('SYSTEM_NAME', '');
 				$TBS->VarRef['timezone'] = ifdef('TIMEZONE', '');
 				$TBS->VarRef['username'] = $_SESSION['user']['username'];
@@ -47,7 +52,7 @@ class Call_spreadsheet_merge extends Call
 				$TBS->VarRef['last_name'] = $_SESSION['user']['last_name'];
 				$TBS->VarRef['email'] = $_SESSION['user']['email'];
 
-				$merge_type = array_get($_REQUEST, 'merge_type', 'person');		
+				$merge_type = array_get($_REQUEST, 'merge_type', 'person');
 				$TBS->VarRef['dates'] = $this->TabulatedData($TBS, 'dates', 60, 'date');
 				$TBS->VarRef['groups'] = $this->TabulatedData($TBS, 'groups', 20, 'group');
 				if (isset($_REQUEST['tables'])) {
@@ -55,11 +60,14 @@ class Call_spreadsheet_merge extends Call
 					foreach ($tables as $table) {
 						$TBS->VarRef[$table.'s'] = $this->TabulatedData($TBS, $table);
 					}
-				} 
+				}
 				if ($merge_type != 'family') { $merge_type = 'person'; }
 				$data = $this->getMergeData();
 				$TBS->MergeBlock($merge_type, $data);
 				$filename = basename($file_info['name']);
+				$bits = explode('.', $filename);
+				$ext = array_pop($bits);
+				$filename = implode('_', $bits).'_merged_'.date('Y-m-d').'.'.$ext;
 				$TBS->Show(OPENTBS_DOWNLOAD, $filename);
 				break;
 
@@ -80,7 +88,7 @@ class Call_spreadsheet_merge extends Call
 				$i++;
 				$TBS->VarRef[$base.$i] = $bit;
 			}
-		} 
+		}
 		$j = $i;
 		while ($i <= $max) {
 			$i++;
@@ -88,7 +96,7 @@ class Call_spreadsheet_merge extends Call
 		}
 		return $j;
 	}
-	
+
 	protected function getMergeData()
 	{
 		$return_data = array();
@@ -160,10 +168,16 @@ class Call_spreadsheet_merge extends Call
 				if ($k == 'history') continue;
 				if ($k == 'familyid') continue;
 				if ($k == 'feed_uuid') continue;
+
+				// NB below we use keys from $headerrow, such as "suburb"
+				// AND raw keys such as "address_suburb", to support both new and legacy formats.
+
 				if ($dummy->hasField($k)) {
-					$outputrow[$headerrow[$k]] = str_replace("\n", ' ',$dummy->getFormattedValue($k, $v)); // pass value to work around read-only fields
+					$outputrow[$headerrow[$k]] = $outputrow[$k] = str_replace("\n", ' ',$dummy->getFormattedValue($k, $v)); // pass value to work around read-only fields
 				} else if ($dummy_family && $dummy_family->hasField($k)) {
-					$outputrow[$headerrow[$k]] = $dummy_family->getFormattedValue($k, $v);
+					$outputrow[$headerrow[$k]] = $outputrow[$k] = $dummy_family->getFormattedValue($k, $v);
+				} else if ($k == 'selected_firstnames') {
+					$outputrow['selected_members'] = $v;
 				} else {
 					$outputrow[$k] = $v;
 				}
@@ -206,4 +220,143 @@ class Call_spreadsheet_merge extends Call
 		}
 		return $return_data;
 	}
+
+
+	function runLegacy()
+	{
+		$source_file = array_get($_FILES, 'source_document');
+		$content = null;
+		if (empty($source_file['tmp_name'])) {
+			trigger_error('Template file does not seem to have been uploaded');
+			return;
+		}
+		$extension = @strtolower(end(explode('.', $source_file['name'])));
+		$source_file = $source_file['tmp_name'];
+		$merged_file = dirname($source_file).'/jethro_merged_'.time().session_id();
+
+		switch ($extension) {
+			case 'odt':
+				$this->mergeLegacyODT($source_file, $merged_file);
+				break;
+
+			case 'docx':
+				$this->mergeLegacyDOCX($source_file, $merged_file);
+				break;
+
+			default:
+				trigger_error("Format $extension not yet supported");
+				return;
+		}
+
+		if (file_exists($merged_file)) {
+
+			header('Content-type: application/force-download');
+			header("Content-Type: application/octet-stream");
+			header("Content-Type: application/download");
+			header('Content-Disposition: attachment; filename="'.urlencode('MERGED_'.basename($_FILES['source_document']['name'])).'"');
+			header('Content-Length: '. filesize($merged_file));
+
+			readfile($merged_file);
+			unlink($merged_file);
+			flush();
+		}
+	}
+
+
+	function mergeLegacyDOCX($source_file, $merged_file)
+	{
+		// Important: we get the merge data first, because the phpWord
+		// autoloader included below stuffs up the Jethro autoloader
+		// and causes errors.
+		$data = array_values($this->getMergeData());
+
+		require_once 'vendor/autoload.php';
+		\PhpOffice\PhpWord\Settings::setTempDir(sys_get_temp_dir());
+		\PhpOffice\PhpWord\Settings::setOutputEscapingEnabled(TRUE);
+		$templateProcessor = new \PhpOffice\PhpWord\TemplateProcessor($source_file);
+
+		$vars = $templateProcessor->getVariables();
+		if (!$templateProcessor->cloneBlock('MERGEBLOCK', count($data))) {
+			if (empty($vars)) {
+				trigger_error("You don't seem to have included any \${keywords} in your file; cannot merge");
+				return;
+			}
+			$templateProcessor->cloneRow(reset($vars), count($data));
+
+		}
+		foreach ($data as $num => $row) {
+			$setVars = Array();
+			foreach ($row as $k => $v) {
+				$templateProcessor->setValue(strtoupper($k).'#'.($num+1), $this->xmlEntities($v));
+				$setVars[strtoupper($k)] = TRUE;
+			}
+			// Set blank value for any remaining keywords
+			foreach ($vars as $v) {
+				if (!isset($setVars[strtoupper($v)])) {
+					$templateProcessor->setValue(strtoupper($v).'#'.($num+1), '');
+				}
+			}
+		}
+		$templateProcessor->saveAs($merged_file);
+	}
+
+	function mergeLegacyODT($source_file, $merged_file)
+	{
+		$xml_filename = 'content.xml';
+		require_once 'include/odf_tools.class.php';
+
+		$content = ODF_Tools::getXML($source_file, $xml_filename);
+		$keywords = ODF_Tools::getKeywords($source_file, $xml_filename);
+		if (empty($content)) {
+			trigger_error('Could not find content within this '.$extension.' file');
+			return;
+		}
+		$HEADER_END = '</text:sequence-decls>';
+		$FOOTER_START = '</office:text>';
+
+		$middle_start_pos = strpos($content, $HEADER_END)+strlen($HEADER_END);
+		$middle_end_pos = strpos($content, $FOOTER_START);
+		if ((NULL === $middle_start_pos) || (NULL === $middle_end_pos)) {
+			trigger_error('Cannot locate body content of the file');
+			return;
+		}
+		$middle_template = substr($content, $middle_start_pos, ($middle_end_pos - $middle_start_pos));
+		$header = substr($content, 0, $middle_start_pos);
+		$footer = substr($content, $middle_end_pos);
+
+		$merged_middle = '';
+
+		foreach ($this->getMergeData() as $id => $row) {
+			if (empty($row)) continue;
+			$this_middle = $middle_template;
+			$replaced = Array();
+			foreach ($row as $k => $v) {
+				$this_middle = str_replace('%'.strtoupper($k).'%', ODF_Tools::odfEntities(trim($v)), $this_middle);
+				$replaced[strtoupper($k)] = TRUE;
+			}
+			// replace keywords without values with blanks
+			foreach ($keywords as $k) {
+				if (!isset($replaced[strtoupper($k)])) {
+					$this_middle = str_replace('%'.strtoupper($k).'%', '', $this_middle);
+				}
+			}
+			$merged_middle .= $this_middle;
+		}
+
+		$merged_file = dirname($source_file).'/jethro_merged_'.time().session_id();
+		copy($source_file, $merged_file);
+
+		ODF_Tools::setXML($merged_file, $header.$merged_middle.$footer, $xml_filename);
+	}
+
+	protected function xmlEntities($x)
+	{
+		$res = str_replace("&", '&amp;', $x);
+		$res = str_replace("'", '&apos;', $res);
+		$res = str_replace('"', '&quot;', $res);
+		$res = str_replace('>', '&gt;', $res);
+		$res = str_replace('<', '&lt;', $res);
+		return $res;
+	}
+
 }
---
+++ calls/call_document_merge_help.class.php
@@ -0,0 +1,242 @@
+<?php
+class Call_Opentbs_merge_help extends Call
+{
+	function run()
+	{
+		?>
+		<html>
+			<head>
+				<?php include 'templates/head.template.php'; ?>
+			</head>
+			<body>
+				<div id="body">
+				<h1>Mail Merge in Jethro</h1><a id="top"></a>
+				<p>Jethro can merge person and family data into Microsoft office files (DOCX, XLS, PPTX) and OpenOffice/LibreOffice files (ODT, ODS, ODP).</p>
+				<p>From version 2.28 onwards, Jethro uses the <a href="http://www.tinybutstrong.com/opentbs.php">TinyButStrong OpenTBS engine</a> to merge documents. However legacy templates ODT/DOCX templates created for older versions of Jethro are still supported, by selecting the "legacy" option.</p>
+
+				<h3>Sample Templates</h3>
+				<h4>For merging person-by-person</h4>
+				<table class="table table-bordered">
+					<tr>
+						<td>Simple list of people in a table</td>
+						<td class="nowrap">
+							<a href="./resources/merge_samples/example_list_of_people.ods">ODS</a> &nbsp;
+							<a href="./resources/merge_samples/example_list_of_people.xlsx">XLSX</a> &nbsp;
+							<a href="./resources/merge_samples/example_list_of_people.odt">ODT</a> &nbsp;
+							<a href="./resources/merge_samples/example_list_of_people.docx">DOCX</a>
+						</td>
+					</tr>
+					<tr>
+						<td>Nametags, A4, 3x7 per page</td>
+						<td>
+							<a href="./resources/merge_samples/example_nametags_7x3.docx">DOCX</a>
+						</td>
+					</tr>
+					<tr>
+						<td>Attendance list, with birthdays highlighted
+							<br /><small><i>This also shows date of birth (a custom field) and whether they have had a birthday in the past week. The ODS version uses conditional formatting to omit dividing lines between members of the same family.</i></small></td>
+						<td>
+							<a href="./resources/merge_samples/example_attendance_sheet.odt">ODS</a> &nbsp;
+							<a href="./resources/merge_samples/example_attendance_sheet.xlsx">XLSX</a>
+						</td>
+					</tr>
+					<tr class="archived">
+						<td>Legacy-format examples</td>
+						<td>
+							<a href="./resources/merge_samples/legacy_merge_sample_persons.odt">ODT</a> &nbsp;
+							<a href="./resources/merge_samples/legacy_merge_sample_persons.docx">DOCX</a>
+						</td>
+					</tr>
+				</table>
+				<h4>For merging family-by-family</h4>
+				<table class="table table-bordered">
+					<tr>
+						<td>Mailing labels, A4, 3x7 per page</td>
+						<td>
+							<a href="./resources/merge_samples/example_mailing_labels_7x3.docx">DOCX</a>
+						</td>
+					</tr>
+					<tr>
+						<td>Family attendance sheet</td>
+						<td>
+							<a href="./resources/merge_samples/example_family_attendance.ods">ODS</a> &nbsp;
+							<a href="./resources/merge_samples/example_family_attendance.xlsx">XLSX</a>
+						</td>
+					</tr>
+
+					<tr class="archived">
+						<td>Legacy-format examples</td>
+						<td>
+							<a href="./resources/merge_samples/legacy_merge_sample_family.odt">ODT</a> &nbsp;
+							<a href="./resources/merge_samples/legacy_merge_sample_family.docx">DOCX</a>
+						</td>
+					</tr>
+				</table>
+				<p>NB You can also generate family listings using Jethro's <a target="_parent" href="?view=families__contact_list">Families - Contact List page</a></p>
+
+				<h3>Creating Templates using TBS tags</h3>
+
+				<p>When creating a template, you include special markers (<em>TBS tags</em>) which will be replaced with the relevant person/family details.<p>
+				<p>A <em>TBS block</em> is defined by one or more <em>TBS fields</em>.  When the merge is performed, the TBS block will be repeated for each person/family.</p>
+
+				The available fields are:
+				<ul>
+					<li><code>[person.id]</code> - the unique internal identifier number</li>
+					<li><code>[person.first_name]</code></li>
+					<li><code>[person.last_name]</code></li>
+					<li><code>[person.gender]</code> - Male/Female/Unknown</li>
+					<li><code>[person.age_bracket]</code></li>
+					<li><code>[person.congregation]</code></li>
+					<li><code>[person.status]</code></li>
+					<li><code>[person.email]</code></li>
+					<li><code>[person.home_tel]</code></li>
+					<li><code>[person.mobile_tel]</code></li>
+					<li><code>[person.work_tel]</code></li>
+					<li><code>[person.contact_remarks]</code></li>
+					<li><code>[person.date_of_last_status_change]</code></li>
+					<li><code>[person.created_date]</code> - when added to the system</li>
+					<li><code>[person.family_name]</code></li>
+					<li><code>[person.address_street]</code></li>
+					<li><code>[person.address_suburb]</code></li>
+					<li><code>[person.address_state]</code></li>
+					<li><code>[person.address_postcode]</code></li>
+
+					<?php
+					$fields = $GLOBALS['system']->getDBObjectData('custom_field', Array());
+						foreach ($fields as $id => $field) {
+							?>
+							<li><code>[person.<?php echo $fname = strtoupper(str_replace(' ', '_', $field['name'])); ?>]</code> -  custom field</li>
+							<?php
+						}
+						?>
+
+					<li><code>[family.id]</code> - the unique internal identifier number</li>
+					<li><code>[family.family_name]</code></li>
+					<li><code>[family.street_address]</code></li>
+					<li><code>[family.suburb]</code></li>
+					<li><code>[family.state]</code></li>
+					<li><code>[family.postcode]</code></li>
+					<li><code>[family.home_tel]</code></li>
+					<li><code>[family.mobile_tels]</code> - mobile numbers of the adults</li>
+					<li><code>[family.emails]</code> - email addresses of the adults</li>
+					<li><code>[family.status]</code></li>
+					<li><code>[family.created_date]</code> - when added to the system</li>
+					<li><code>[family.members]</code> - first names</li>
+					<li><code>[family.members_full]</code> - full names</li>
+					<li><code>[family.adult_members]</code> - first names</li>
+					<li><code>[family.adult_members_full]</code> - full names</li>
+					<li><code>[family.selected_firstnames]</code> - selected on the list generating the document</li>
+					<li><code>[family.selected_lastnames]</code> - selected on the list generating the document</li>
+					<li><code>[family.selected_names]</code> - full names</li>
+				</ul>
+				<br />
+
+				<p>You define a <em>TBS Block</em> by adding <code>;block=tbs:row</code> to the first field in the block, eg <code>[person.first_name;block=tbs:row]</code></p>
+
+				<p>There are also some special fields which you may use outside a TBS block:</p>
+				<ul>
+					<li><code>[onshow..now;frm='yyyy-mm-dd']</code> - the current date and time</li>
+				<li><code>[onshow.system_name]</code> - the name of Jethro system</li>
+				<li><code>[onshow.username]</code> - the username of the person generating the document</li>
+				<li><code>[onshow.first_name]</code> - the first name of the person generating the document</li>
+				<li><code>[onshow.last_name]</code> - the last name of the person generating the document</li>
+				<li><code>[onshow.email]</code> - the email address of the person generating the document</li>
+				</ul>
+
+				<h3>Extra Fields for Attendance Page</h3>
+				<p>When merging a document from the 'Display attendance' screen, with "tabular" format selected, additional fields are available:
+				<ul>
+				<li><code>[onshow.date1]</code>...<code>[onshow.date60]</code> - for each date selected</li>
+				<li><code>[onshow.dates]</code> - the number of dates selected</li>
+				<li><code>[onshow.group1]</code>...<code>[onshow.group60]</code> - for each group/congregation selected</li>
+				<li><code>[onshow.groups]</code> - the number of groups/congregations selected</li>
+				<li><code>[onshow.headcount1]</code>...<code>[onshow.headcount60]</code> - for each column that has a head count</li>
+				<li><code>[onshow.headcounts]</code> - the number of columns with a head count</li>
+				<li><code>[onshow.present1]</code>...<code>[onshow.present60]</code> - for each column that has a total present</li>
+				<li><code>[onshow.presents]</code> - the number of columns with a total present</li>
+				<li><code>[onshow.absent1]</code>...<code>[onshow.absent60]</code> - for each column that has a total absent</li>
+				<li><code>[onshow.absents]</code> - the number of columns with a total absent</li>
+				<li><code>[onshow.extra1]</code>...<code>[onshow.extra60]</code> - for each column that has a total of extras</li>
+				<li><code>[onshow.extras]</code> - the number of columns with a total of extras</li>
+				</ul>
+				<p>Note: Columns with a total of zero are included.</p>
+				<p>
+				<b>Example Attendance-Specific Templates</b>:<br />  Example 1:
+							<a href="./resources/merge_samples/example_monthly_attendance.ods">ODS</a> &nbsp;
+							<a href="./resources/merge_samples/example_monthly_attendance.xlsx">XLSX</a> &nbsp;
+				Example 2:
+							<a href="./resources/merge_samples/example_monthly_attendance1.ods">ODS</a> &nbsp;
+							<a href="./resources/merge_samples/example_monthly_attendance1.xlsx">XLSX</a> &nbsp;
+				Example 3:
+							<a href="./resources/merge_samples/example_monthly_attendance2.ods">ODS</a> &nbsp;
+							<a href="./resources/merge_samples/example_monthly_attendance2.xlsx">XLSX</a> &nbsp;
+				</p>
+
+				<h3>Extra Fields for Rosters Page</h3>
+				<p>When merging a document from the 'Display roster assignments' screen, the following additional fields are available.
+				<ul>
+				<li><code>[onshow.roster_view_name]</code> - the name of the roster view</li>
+				<li><code>[onshow.date]</code> - the date of the last roster</li>
+				<li><code>[onshow.label1]</code>...<code>[onshow.label20]</code> - label for each column of the roster</li>
+				</ul>
+				Block fields
+				<ul>
+				<li><code>[roster.date]</code> - the date the roster</li>
+				<li><code>[roster.role1]</code>...<code>[roster.role20]</code> - the person(s) doing the role on that date</li>
+				<li><code>[people.date]</code> - the date the person is on the roster</li>
+				<li><code>[people.name]</code> - the person's name</li>
+				<li><code>[roster.role1]</code>...<code>[roster.role20]</code> - the person(s) doing the role on that date</li>
+				</ul>
+
+				<h3>Controlling Spreadsheet Cell Formatting</h3>
+				<p>When merging a spreadsheet, cells may be formatted and also typed. For example, a cell value may be typed as String, Numerical, Boolean or Date.</p>
+				<p>Whenever you enter a TBS tag into a cell, the cell will be typed as String. TBS offers special parameters to change the cell type.</p>
+				<h5>Example:</h5>
+				<ul>
+					<li><code>[onshow.presents;ope=tbs:num]</code> - will make the cell Numeric after the number of people present is inserted.</li>
+				</ul>
+				<h5>Parameters for merging   data in spreadsheet cells:</h5>
+				<table class="table table-bordered">
+				  <tbody><tr>
+					<th align="left">Required Cell Type</th>
+					<th class="centered">Parameter</th>
+					<th class="centered">Note</th>
+				  </tr>
+				  <tr>
+					<td nowrap="nowrap">Number</td>
+					<td class="smallcode" nowrap="nowrap">ope=tbs:num</td>
+					<td class="smallcode" valign="top" nowrap="nowrap">&nbsp;</td>
+				  </tr>
+				  <tr>
+					<td nowrap="nowrap">Boolean</td>
+					<td class="smallcode" nowrap="nowrap">ope=tbs:bool</td>
+					<td class="smallcode" valign="top" nowrap="nowrap">&nbsp;</td>
+				  </tr>
+				  <tr>
+					<td nowrap="nowrap">Date/time</td>
+					<td class="smallcode" nowrap="nowrap">ope=tbs:date</td>
+					<td class="smallcode" valign="top" nowrap="nowrap">&nbsp;</td>
+				  </tr>
+				  <tr>
+					<td nowrap="nowrap">Time only</td>
+					<td class="smallcode" nowrap="nowrap">ope=tbs:time</td>
+					<td nowrap="nowrap">For XLSX, it's an alias of <span class="smallcode">ope=tbs:date</span></td>
+				  </tr>
+				  <tr>
+					<td nowrap="nowrap">Currency</td>
+					<td class="smallcode" nowrap="nowrap">ope=tbs:curr</td>
+					<td nowrap="nowrap"><p>For XLSX, it's an alias of <span class="smallcode">ope=tbs:num</span></p></td>
+				  </tr>
+				  <tr>
+					<td nowrap="nowrap">Percentage</td>
+					<td class="smallcode" nowrap="nowrap">ope=tbs:percent</td>
+					<td nowrap="nowrap">For XLSX, it's an alias of <span class="smallcode">ope=tbs:num</span></td>
+				  </tr>
+				</tbody></table>
+		</div>
+		</body>
+		</html>
+		<?php
+	}
+}
+?>
