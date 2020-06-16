1cbf1c7ab5fd5c94c11cc6e17085ed593104a3dc
tbar0970 <tom@easyjethro.com.au>
2020-06-16 10:55:38 +1000

    Remove the SLIDES_ALL constant. New rule: Any item which shows in the handout, with content, shows in the slides. Can enhance this later if the need arises.

--- calls/call_service_slides.class.php
+++ calls/call_service_slides.class.php
@@ -1,7 +1,7 @@
 <?php
 
 class Call_Service_slides extends Call
-{	
+{
 	function run()
 	{
 		//get service data from database
@@ -13,7 +13,7 @@ class Call_Service_slides extends Call
 		$date = str_replace(' ', '',$service->getFormattedValue("date"));
 		$cong = $service->getFormattedValue("congregationid");
 		$outFileName = 'Slides_'.$date.'_'. $cong.'.odp';
-		
+
 		$templateFilename = Documents_Manager::getRootPath().'/Templates/slide_template.odp';
 		$outname = Documents_Manager::getRootPath().'/'.$outFileName;
 		if (!file_exists($templateFilename)) {
@@ -31,41 +31,37 @@ class Call_Service_slides extends Call
 				} else {
 					echo 'failed to open template';
 				}
-				
+
 				$dom = new DOMDocument('1.0', "UTF-8");
 				$dom->loadXML($xml);
-				
+
 				//Set up DOM & Namespaces
 				$xpath = new DomXPath($dom);
 				$xpath->registerNamespace("draw","urn:oasis:names:tc:opendocument:xmlns:drawing:1.0");
 				$xpath->registerNamespace("presentation","urn:oasis:names:tc:opendocument:xmlns:office:1.0");
-				
+
 				//Get pages in template
 				$pages = $xpath->query("//office:presentation/node()");
 				$last = $pages->length; //used later to remove templates
-				
+
 				//Define Template slides in file:
 				$blank = $pages->item(0);
 				$template = $pages->item(1);
 				$alternate = $pages->item(2);  // alternate for identical slides (eg italic)
-				
+
 				//starting slideid
 				$slideid = 10;
-
 				//Count service items to deal with
 				$numitems = count($serviceContent);
-				//loop for each 
+				//loop for each
 				for($x = 0; $x < $numitems; $x++) {
+					if (!strlen($serviceContent[$x][1])) continue; // no content
+
 					//Split off content type (eg song, prayer, etc)
 					$title = explode(": ",$serviceContent[$x][0],2);
-					
-					//Skip non-songs if option is set
-					if (SLIDES_ALL == 0 and ($title[0] !== 'Song')) { 
-						continue;
-					} elseif (SLIDES_ALL == 1 and ($title[0] !== 'Song')){
-						$title[0] = ($serviceContent[$x][0]);
-					}
-					
+
+					$title[0] = ($serviceContent[$x][0]);
+
 					//Break song into verses (<p> tag)
 					$verses = explode ("</p>",$serviceContent[$x][1]);
 					//reset variable for checking if slides are repeated
@@ -79,17 +75,17 @@ class Call_Service_slides extends Call
 						} else {
 							$newpage = $template->cloneNode(true);
 						}
-						
+
 						$newslide = $template->parentNode->insertBefore($newpage,$pages->item($last-1));
 						$newslide->setAttribute('draw:name',('Slide'.($slideid)));//Set slide ID
 						$slideid++;
-						
+
 						//Get all text lines on slide & loop
 						$textelements = $xpath->query(".//text:span",$newslide);
 						$numtextelements = $textelements->length;
-						
-						
-						
+
+
+
 						for($y = 0; $y < $numtextelements; $y++) {
 							if (strcmp($textelements->item($y)->nodeValue, "title") == 0) { //title textbox
 								$textelements->item($y)->nodeValue = array_values(array_slice($title, -1))[0]; //get last element of title array
@@ -100,9 +96,9 @@ class Call_Service_slides extends Call
 								$lines = array();
 								$lines = explode('<br />',$verses[$a]);
 								$numlines = count($lines);
-								
+
 								//clone nodes for each line of text
-								for ($z = 1; $z < ($numlines); $z++) {	
+								for ($z = 1; $z < ($numlines); $z++) {
 									$line = $textelements->item($y)->parentNode->cloneNode(true);
 									$newline = $textelements->item($y)->parentNode->parentNode->appendChild($line);
 								}
@@ -111,23 +107,23 @@ class Call_Service_slides extends Call
 								//populate text elements
 								for ($z = 0; $z < ($numlines); $z++) {
 									$textlines->item($z)->nodeValue = htmlspecialchars(strip_tags(html_entity_decode($lines[$z])), ENT_QUOTES, 'UTF-8', false);
-								}	
-								
+								}
+
 							} elseif (strcmp($textelements->item($y)->nodeValue, 'credit') == 0) { //credits textbox
 
 								$slideNumText = 'Slide '.($a+1).' of '.($numVerses);
-								
+
 								if ($a < $numVerses-1) {
 									$textelements->item($y)->nodeValue = $slideNumText;
 								} else {
 									//deal with multiline text
 									$lines = array_filter(array_merge(array($slideNumText),explode('<br />',$serviceContent[$x][2])));
 									$numlines = count($lines);
-								
+
 									//clone nodes for each line of text
-									for ($z = 0; $z < ($numlines-1); $z++) {	
+									for ($z = 0; $z < ($numlines-1); $z++) {
 										$line = $textelements->item($y)->parentNode->cloneNode(true);
-										$newline = $textelements->item($y)->parentNode->parentNode->appendChild($line);								
+										$newline = $textelements->item($y)->parentNode->parentNode->appendChild($line);
 									}
 									//find text elements within cloned nodes above
 									$textlines = $xpath->query(".//*[text()[contains(., 'credit')]]",$textelements->item($y)->parentNode->parentNode);
@@ -137,9 +133,9 @@ class Call_Service_slides extends Call
 										$textlines->item($z)->nodeValue = htmlspecialchars(strip_tags(html_entity_decode($lines[$z], ENT_XML1, 'UTF-8')), ENT_QUOTES, 'UTF-8', false);
 									}
 								}
-							} 
+							}
 						}
-				
+
 						$dom->saveXML();
 						$previousVerse = $verses[$a];
 					}
@@ -149,19 +145,19 @@ class Call_Service_slides extends Call
 					$newslide->setAttribute('draw:name',('Slide'.($slideid)));//Set slide ID
 					$slideid++;
 				}
-				
+
 				//remove original template slides
 				for ($z = 0; $z < ($last-1); $z++) {
 					$pages->item($z)->parentNode->removeChild($pages->item($z));
 				}
-				
+
 				//save xml & zip
 				$result = $dom->saveXML();
-				
+
 				if(!copy($templateFilename, $outname)) {
 					die("Could not copy '$templateFilename' to '$outname'");
 				}
-								
+
 				if ($zip->open($outname) === TRUE) {
 					$zip->deleteName($fileToModify);
 					$zip->addFromString($fileToModify, $result);
@@ -181,4 +177,3 @@ class Call_Service_slides extends Call
 		}
 	}
 }
-?>
