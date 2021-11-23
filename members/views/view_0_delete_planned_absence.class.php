3add3cb0f45bc4741afa37df929699b0f6845fb3
Tom Barrett <tom@easyjethro.com.au>
2021-11-23 11:16:27 +1100

    Planned absences in the member area

---
+++ members/views/view_0_delete_planned_absence.class.php
@@ -0,0 +1,33 @@
+<?php
+class View__Delete_Planned_Absence extends View
+{
+	function processView()
+	{
+		$absence = new Planned_Absence((int)$_REQUEST['id']);
+
+		$person = new Person($GLOBALS['user_system']->getCurrentPerson('id'));
+		$family = new Family($person->getValue('familyid'));
+		$fmembers = $family->getMemberData();
+		if (!isset($fmembers[$absence->getValue('personid')])) {
+			trigger_error(E_USER_ERROR, 'Attempt to delete absence for a person outside the users family');
+			exit;
+		}
+
+		if ($absence && $absence->delete()) {
+			add_message('Planned absence deleted', 'success');
+			redirect(-1, Array(), 'rosters');
+		} else {
+			add_message("Error while deleting planned absence");
+		}
+	}
+
+	function printView()
+	{
+	}
+
+	public function getTitle()
+	{
+		return _('Delete ').$this->_person->toString();
+	}
+
+}
