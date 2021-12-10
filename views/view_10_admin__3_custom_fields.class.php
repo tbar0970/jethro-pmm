4b7946c71e11f24692f6138c3649e8357f0363e2
Tom Barrett <tom@tombarrett.id.au>
2021-12-10 14:49:11 +1100

    Fix comparison function - recent php more fussy

--- views/view_10_admin__3_custom_fields.class.php
+++ views/view_10_admin__3_custom_fields.class.php
@@ -57,7 +57,7 @@ class View_Admin__Custom_Fields extends View
 
 
 
-		uasort($this->fields, function($x,$y) {return (int)$x->getValue("rank") > (int)$y->getValue("rank");});
+		uasort($this->fields, function($x,$y) {return ((int)$x->getValue("rank") > (int)$y->getValue("rank")) ? 1 : 0;});
 
 		if ($i > 0) {
 			add_message("Custom fields updated", 'success');
