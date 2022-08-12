20887de9ac5065232a7e1fd2eb5641aa9d1d2849
Tom Barrett <tom@tombarrett.id.au>
2022-08-12 13:22:59 +1000

    Fix #813 in the wild by recreating the planned_absence table with correct FKs.

--- upgrades/2022-upgrade-to-2.32.sql
+++ upgrades/2022-upgrade-to-2.32.sql
@@ -21,3 +21,26 @@ UPDATE _person_group
 SET categoryid = NULL where categoryid not in (select id from person_group_category);
 
 alter table _person_group add constraint `person_group_cat` foreign key (`categoryid`) REFERENCES `person_group_category` (`id`) ON DELETE SET NULL;
+
+/* issue 813 - fix (or add) FKs on the planned_absence table */
+
+CREATE TABLE planned_absence_temp select * from planned_absence;
+DROP TABLE planned_absence;
+
+CREATE TABLE `planned_absence` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `personid` int(11) NOT NULL DEFAULT '0',
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `comment` text NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `creator` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `planned_absencecreator` FOREIGN KEY (`creator`) REFERENCES `_person` (`id`) ON DELETE RESTRICT,
+  CONSTRAINT `planned_absencepersonid` FOREIGN KEY (`personid`) REFERENCES `_person` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO planned_absence
+SELECT * from planned_absence_temp;
+
+DROP TABLE planned_absence_temp;
