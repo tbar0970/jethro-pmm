e1c77612b1ef1adc341a24fbd60b93f4de542a98
tbar0970 <tom@tombarrett.id.au>
2023-02-06 22:12:21 +1100

    Fix #821

--- members/templates/account_request_received.template.php
+++ members/templates/account_request_received.template.php
@@ -9,13 +9,11 @@
 			<h1><?php echo ents(SYSTEM_NAME); ?></h1>
 		</div>
 		<div id="login-body">
-			<h3>Email sent</h3>
+			<h3><i class="icon-ok-sign"></i> Account request received</h3>
 
-			<p>Please read the email that we have sent you, and click the link inside the email to finish creating your account.</p>
+			<p>If your email address is recognised by our system, you'll soon receive an email with a link to activate your account.</p>
 
-			<p>If the email hasn't arrived after a few minutes, please check your junk or spam folders.</p>
-			
-			<p>If you still haven't received the email, it might be because your email address is not yet recorded in our database.  
+			<p>If the email hasn't arrived after a few minutes, please check your junk or spam folders. Or if you think we might have a different email address on file, try again using that one.</p>
 
 			<?php
 			if (defined('MEMBER_REGO_HELP_EMAIL')) {
