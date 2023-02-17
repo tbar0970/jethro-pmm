9d0f01544b56084575c8dd156e9a738d69e23c6c
tbar0970 <tom@tombarrett.id.au>
2023-02-17 21:37:28 +1100

    Issue #504 - add MEMBER_REGO_HELP_EMAIL setting to database so it can be edited

--- upgrades/2023-upgrade-to-2.33.sql
+++ upgrades/2023-upgrade-to-2.33.sql
@@ -1,1 +1,5 @@
 delete from setting where symbol = 'ATTENDANCE_LIST_ORDER';
+delete from setting where symbol = 'MEMBER_REGO_EMAIL_CC';
+insert into setting (`rank`, symbol, note, type, value)
+select `rank`-1, 'MEMBER_REGO_HELP_EMAIL', 'Address that users can contact for assistance with member rego (optional)', 'text', ''
+from setting where symbol = 'MEMBER_REGO_FAILURE_EMAIL'; 
