e39889c55da25a7844336b622d0b3e75dcb00479
tbar0970 <tom@tombarrett.id.au>
2023-11-10 19:43:41 +1100

    Fix #963

--- upgrades/2023-upgrade-to-2.34.sql
+++ upgrades/2023-upgrade-to-2.34.sql
@@ -23,16 +23,16 @@ WHERE
         (`p`.`id` in (select `m`.`personid` AS `personid` from (`person_group_membership` `m` join `account_group_restriction` `gr` on((`m`.`groupid` = `gr`.`groupid`))) where (`gr`.`personid` = `getCurrentUserID`())))
     );
 
-INSERT INTO setting (rank, heading, symbol, note, type, value)
-SELECT rank-1, 'iCal Feeds', 'ROSTER_FEEDS_ENABLED', 'Whether users can access their roster assignments via an ical feed with secret URL', 'bool', 1
+INSERT INTO setting (`rank`, heading, symbol, note, type, value)
+SELECT `rank`-1, 'iCal Feeds', 'ROSTER_FEEDS_ENABLED', 'Whether users can access their roster assignments via an ical feed with secret URL', 'bool', 1
 FROM setting WHERE symbol = 'PUBLIC_AREA_ENABLED';
 
-INSERT INTO setting (rank, heading, symbol, note, type, value)
-SELECT rank-1, '', 'DEFAULT_NOTE_STATUS', 'Default status when creating a new note', 'select{"no_action":"No Action Required","pending":"Requires Action"}', 'pending'
+INSERT INTO setting (`rank`, heading, symbol, note, type, value)
+SELECT `rank`-1, '', 'DEFAULT_NOTE_STATUS', 'Default status when creating a new note', 'select{"no_action":"No Action Required","pending":"Requires Action"}', 'pending'
 FROM setting WHERE symbol = 'NOTES_ORDER';
 
-SELECT @newrank:=rank+1 from setting where symbol = 'NOTES_ORDER';
-UPDATE setting SET rank = @newrank WHERE symbol = 'NOTES_LINK_TO_EDIT';
+SELECT @newrank:=`rank`+1 from setting where symbol = 'NOTES_ORDER';
+UPDATE setting SET `rank` = @newrank WHERE symbol = 'NOTES_LINK_TO_EDIT';
 
 
 UPDATE roster_role set volunteer_group = NULL
