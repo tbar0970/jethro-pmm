a5b9d5ccebb3bac04c6410b863547ead64c5afe2
Jeff Turner <jeff@redradishtech.com>
2025-09-19 21:18:48 +1000

    Fix CCLI SongSelect URLs. Fixes #1227

--- upgrades/2025-upgrade-to-2.37.sql
+++ upgrades/2025-upgrade-to-2.37.sql
@@ -5,3 +5,7 @@ update setting set value=160 where symbol='SMS_MAX_LENGTH' and value=140;
 --  (any existing null values become 1 Jan 2000)
 UPDATE roster_role_assignment SET assignedon="2000-01-01" where assignedon IS NULL;
 ALTER TABLE roster_role_assignment MODIFY COLUMN assignedon TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
+
+-- #1227 - fix CCLI song links
+update setting set value='https://songselect.ccli.com/songs/__NUMBER__' where value='https://au.songselect.com/songs/__NUMBER__' and  symbol='CCLI_DETAIL_URL';
+update setting set value='https://songselect.ccli.com/search/results?search=__TITLE__' where value='http://us.search.ccli.com/search/results?SearchText=__TITLE__' and  symbol='CCLI_SEARCH_URL';
